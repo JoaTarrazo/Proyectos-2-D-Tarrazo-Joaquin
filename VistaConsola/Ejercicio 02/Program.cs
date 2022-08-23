d207ac506229878827a40d5eaa90ec34516c2804
JoaTarrazo <joatarrazo99@gmail.com>
2022-08-23 16:14:44 -0300

    ejercicio 2

--- VistaConsola/Ejercicio 02/Program.cs
+++ VistaConsola/Ejercicio 02/Program.cs
@@ -13,13 +13,25 @@ namespace Ejercicio_02
 
 
 
+            Console.WriteLine("Ingrese un numero mayor a 0");
 
+            bool comprobacionDelNumeroIngresado = false;
+            double numeroIngresado;
 
+           string numeroIngresadoString = Console.ReadLine();
 
+            comprobacionDelNumeroIngresado = double.TryParse(numeroIngresadoString, out numeroIngresado);
 
-
-
-
+            if(comprobacionDelNumeroIngresado && numeroIngresado>0)
+            {
+              double resultadoCuadrado =  Math.Pow(numeroIngresado, 2); //raiz cuadrada(2) del numero ingresado
+              double resultadoCubo= Math.Pow(numeroIngresado, 3); //raiz cubica(3) del numero ingresado
+                Console.WriteLine($"La raiz cuadrada del numero ingresado es: {resultadoCuadrado} y la raiz al cubo es: {resultadoCubo}");
+            } 
+            else
+            {
+                Console.WriteLine("Error al ingresar el dato, este tiene que ser un numero y ser mayor a 0, intente nuevamente");
+            }
         }
     }
 }
